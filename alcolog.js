c6b60d5554c9d140b82e4a5ec20775244c9b576c
Alexey Berezhnoy <collapsus@gmail.com>
2010-04-03 23:33:51 +0400

    Теперь нельзя нажать Submit при каком-либо незаполненном поле, при нажатии Submit обнуляются инпуты (кроме даты), добавлена анимация коммита, добавлена крепость напитка, и высчитывание количества алкоголя в день, проставляется сегодняшняя дата при старте, добавлена история дня

--- alcolog.js
+++ alcolog.js
@@ -1,23 +1,27 @@
-function AlcoLog(logs){
+function AlcoLog(logs, drinks){
     this.logs = logs;
+    this.drinks = drinks;
     this.makeDOM();
 };
 
 AlcoLog.prototype.makeHTML = function(){
-    return[
+    var r = [
         '<div class="alcolog">',
             '<table>',
                 '<tr>',
                     '<td>',
                         '<input type="text" class="datepicker" />',
                         '<select class="drinkpicker" >',
-                            '<option disabled>Выберите напиток</option>',
-                            '<option value="Пиво">Пиво</option>',
-                            '<option selected value="Вино">Вино</option>',
-                            '<option value="Водка">Водка</option>',
+                            this.makeOptions(this.drinks),
                         '</select>',
+                        '<input type="text" class="percent" readonly size="2" maxlength="2" /> %&nbsp;',
                         '<input type="text" class="volumepicker" size="5" maxlength="5" /> мл.&nbsp;',
-                        '<input type="button" value="Submit" class="submit" />',
+                        '<input type="button" value="Submit" class="submit" disabled="disabled"/>',
+                    '</td>',
+                '</tr>',
+                '<tr>',
+                    '<td>',
+                        '<div class="history"></div>',
                     '</td>',
                 '</tr>',
                 '<tr>',
@@ -28,6 +32,16 @@ AlcoLog.prototype.makeHTML = function(){
             '</table>',
         '</div>'
     ].join('');
+    return r;
+};
+
+AlcoLog.prototype.makeOptions = function(values){
+    var arr = ['<option selected disabled>Выберите напиток</option>'];
+    $.each(values, function(k, v){
+        var s = '<option value="' + k + '">' + k + '</option>';
+        arr.push(s);
+    });
+    return arr.join();
 };
 
 AlcoLog.prototype.makeDOM = function(){
@@ -36,39 +50,90 @@ AlcoLog.prototype.makeDOM = function(){
     $.each({
         datepicker: '.datepicker',
         drinkpicker: '.drinkpicker',
+        percent: '.percent',
         volumepicker: '.volumepicker',
         submit: '.submit',
+        history: '.history',
         commit: '.commit'
     }, function(k, v){
         _this[k] = _this.elem.find(v);
     });
 
-    this.datepicker.datepicker({dateFormat: 'dd.mm.yy', autoSize: true, firstDay: 1});
+    this.datepicker.datepicker({dateFormat: 'dd.mm.yy', autoSize: true, firstDay: 1}).datepicker("setDate", '0');
 
+    this.drinkpicker.change(function(){
+        _this.percent.val(_this.drinks[$(this).val()]);
+        if (($(this).val() != 'Выберите напиток') && (_this.datepicker.val() != '') && (_this.volumepicker.val() != '')) {
+            _this.submit.attr('disabled', '');
+        } else {
+            _this.submit.attr('disabled', 'disabled');
+        };
+    });
 
     this.volumepicker.keypress(function(event){
         return keyDownNumber(event);
+    });
+
+    this.volumepicker.keyup(function(){
+        if (($(this).val() != '') && (_this.datepicker.val() != '') && (_this.percent.val() != '')) {
+            _this.submit.attr('disabled', '');
+        } else {
+            _this.submit.attr('disabled', 'disabled');
+        };
+    });
+
+    this.datepicker.change(function(){
+        if (($(this).val() != '') && (_this.volumepicker.val() != '') && (_this.percent.val() != '')) {
+            _this.submit.attr('disabled', '');
+        } else {
+            _this.submit.attr('disabled', 'disabled');
+        };
+
+        var currentDate = _this.datepicker.val();
+        $('.historyrec').remove();
+        if (_this.logs[currentDate]) {
+            $.each(_this.logs[currentDate], function(k, v){
+                var name = k == 'totalDayAlcohol' ? 'Всего чистого алкоголя за день' : k;
+                $('<div class="historyrec">' + currentDate + ' / ' + name + ': ' + v + 'мл.</div>').appendTo(_this.history);
+                _this.history.show();
+            });
+        };
+
     });
 
     this.submit.click(function(){
         var currentDate = _this.datepicker.val(),
             currentDrink = _this.drinkpicker.val(),
             currentVolume = _this.volumepicker.val();
+            currentPercent = _this.percent.val();
         if (!_this.logs[currentDate]) {
-            _this.logs[currentDate] = {};
+            _this.logs[currentDate] = {'totalDayAlcohol': 0};
         };
         if (!_this.logs[currentDate][currentDrink]) {
             _this.logs[currentDate][currentDrink] = + currentVolume;
         } else {
             _this.logs[currentDate][currentDrink] += + currentVolume;
         };
+        _this.logs[currentDate]['totalDayAlcohol'] += + currentVolume * currentPercent / 100;
+        $(this).attr('disabled', 'disabled');
+        _this.volumepicker.val('');
+        _this.drinkpicker.val('Выберите напиток');
+        _this.percent.val('');
 
-        $(this).attr('disabled', 'disabled').animate({disabled: ''}, 1000);
-        _this.commit.text('Учтено: ' + currentDate + ' / ' + currentDrink + ' / ' + currentVolume + 'мл.').show();
-        setTimeout(function(){ _this.commit.fadeOut() }, 3000);
+            $('.historyrec').remove();
+            if (_this.logs[currentDate]) {
+                $.each(_this.logs[currentDate], function(k, v){
+                    var name = k == 'totalDayAlcohol' ? 'Всего чистого алкоголя за день' : k;
+                    $('<div class="historyrec">' + currentDate + ' / ' + name + ': ' + v + 'мл.</div>').appendTo(_this.history);
+                    _this.history.show();
+                });
+            };
+
+        _this.commit.text('Учтено: ' + currentDate + ' / ' + currentDrink + '('+ currentPercent + '%) / ' + currentVolume + 'мл.').show();
+        setTimeout(function(){ _this.commit.fadeOut('slow', function(){}) }, 3000);
     });
-
 };
+
 
 function keyDownNumber(e){  //цельнотянуто с http://www.gotdotnet.ru/forums/4/47901/
     var key = (typeof e.charCode == 'undefined' ? e.keyCode : e.charCode);
